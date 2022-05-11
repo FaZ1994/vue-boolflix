755cab9e25c3cae6614a950410951b230999b082
Fabrizio Zivolo <faz94@hotmail.it>
2022-05-11 09:51:57 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+ <h1 class="text-center display-6">Boolfix</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+@import './styles/general.scss'
+</style>
