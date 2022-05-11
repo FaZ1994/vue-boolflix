1d82722392917441de989f6d3a39e5dea6130950
Fabrizio Zivolo <faz94@hotmail.it>
2022-05-11 15:55:15 +0200

    milestone 1-2 svolte

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,87 @@
 <template>
   <div id="app">
- <h1 class="text-center display-6">Boolfix</h1>
+    <header>
+       <h1 class="display-6">Boolflix</h1>
+       <search-bar @performSearch="search" />
+    </header>
+    <main>     
+      <grid-list :items="movies" title="Movies" :loader="loading"/>
+      <grid-list :items="series" title="Series" :loader="loadingSeries"/>
+    </main>
+   
   </div>
 </template>
 
 <script>
-
+import GridList from './components/GridList.vue'
+import SearchBar from './components/SearchBar.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    
+    SearchBar,
+    GridList
+  },
+  data(){
+    return{
+      apiKey: 'e99307154c6dfb0b4750f6603256716d',
+      apiPath: 'https://api.themoviedb.org/3/search/',
+      movies:[],
+      series:[],
+      loading: false,
+      loadingSeries: false
+    }
+  },
+  methods:{
+    getMovies(queryParams){
+      axios.get(this.apiPath+'movie', queryParams).then((res)=>{
+        //console.log(res.data.results)
+        this.movies = res.data.results;
+        this.loading = false;
+      }).catch((error)=>{
+        console.log(error);
+      })
+    },
+    getSeries(queryParams){
+      axios.get(this.apiPath+'tv', queryParams).then((res)=>{
+        //console.log(res.data.results)
+        this.series = res.data.results;
+        this.loadingSeries = false;
+      }).catch((error)=>{
+        console.log(error);
+      })
+    },
+    // getSeriesDue(queryParams){
+    //   return axios.get(this.apiPath+'tv', queryParams)
+    // },
+    // getMoviesDue(queryParams){
+    //   return axios.get(this.apiPath+'movie', queryParams)
+    // },
+    // getAll(queryParams){
+    //   Promise.all([this.getMoviesDue(queryParams), this.getSeriesDue(queryParams)]).then(function (results) {
+    //     console.log(results[0]);
+    //      console.log(results[1]);
+    //   });
+    // },
+    search(text){
+      //console.log(text);
+       const queryParams = {
+        params:{
+          api_key: this.apiKey,
+          query: text
+        }
+      }
+      this.loading = true;
+      this.loadingSeries = true;
+      //this.getAll(queryParams);
+      this.getMovies(queryParams);
+      this.getSeries(queryParams);
+    }
   }
-}
+ }
 </script>
 
 <style lang="scss">
-@import './styles/general.scss'
+  @import './styles/general.scss' 
 </style>
---
+++ src/components/GridList.vue
@@ -0,0 +1,54 @@
+
+<template>
+    <section>
+        <h2>{{title}}</h2>
+       <ul>
+        <li v-for="(item) in items" :key="item.id">
+          id: {{item.id}}<br />
+          titolo originale: {{item.original_title ? item.original_title : item.original_name}}<br />
+          titolo: {{item.title ? item.title : item.name}}<br />
+          lingua: <country-flag :country='language(item)' size='small'/><br />
+          voto: {{item.vote_average}}<br />
+        </li>
+      </ul>
+     
+    </section>
+</template>
+
+<script>
+import CountryFlag from 'vue-country-flag'
+//import {state, actions} from '../store.js'
+export default {
+    name: 'GridList',
+    components:{CountryFlag},
+    props:{
+        items: Array,
+        loader: Boolean,
+        title: String
+    },//['items','loader'],
+    data(){
+        return {
+            
+        }
+    },
+    computed: {
+        // mysearch(){
+        //     return state.search
+        // }
+    },
+    methods:{
+        language(film){
+            if(film.original_language === 'en'){
+                return film.original_language = 'gb'
+            }else if(film.original_language === 'ja'){
+                return film.original_language = 'jp'
+            }else{
+                return film.original_language
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+</style>
